3bfd19551b16cb1f188a9cb7d137a4f05892c36a
Anych <anuar123@mail.ru>
2022-08-22 03:51:40 +0600

    Finish to do

--- src/main/java/com/epam/rd/autotasks/triangle/Triangle.java
+++ src/main/java/com/epam/rd/autotasks/triangle/Triangle.java
@@ -1,17 +1,34 @@
 package com.epam.rd.autotasks.triangle;
 
 class Triangle {
+    Point a;
+    Point b;
+    Point c;
 
+    double ax, ay, bx, by, cx, cy;
     public Triangle(Point a, Point b, Point c) {
-        //TODO
+        this.a = a;
+        this.b = b;
+        this.c = c;
+
+        ax = a.getX();
+        ay = a.getY();
+        bx = b.getX();
+        by = b.getY();
+        cx = c.getX();
+        cy = c.getY();
+        if (this.area() == 0) {
+            throw new RuntimeException();
+        }
     }
 
     public double area() {
-        //TODO
+        double area = (ax * (by - cy) + bx * (cy - ay) + cx * (ay - by)) / 2;
+        return Math.abs(area);
     }
 
     public Point centroid(){
-        //TODO
+        return new Point(((ax + bx + cx) / 3), ((ay + by + cy) / 3));
     }
 
 }
--- src/test/java/com/epam/rd/autotasks/triangle/Main.java
+++ src/test/java/com/epam/rd/autotasks/triangle/Main.java
@@ -4,7 +4,7 @@ package com.epam.rd.autotasks.triangle;
 public class Main {
     public static void main(String[] args) {
         {
-            double area = new Triangle(new Point(0, 0), new Point(3, 0), new Point(0, 4)).area();
+            double area = new Triangle(new Point(1, 3), new Point(3, 9), new Point(2, 6)).area();
             System.out.println(area);
         }
         {
